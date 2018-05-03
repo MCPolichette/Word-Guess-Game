7d5f9f45a9a715a6a6fef7f02106b4b1da09f39a
MCPolichette <mark@polichette.com>
2018-05-02 18:58:26 -0600

    wednesday work

--- assets/javascript.js
+++ assets/javascript.js
@@ -7,67 +7,123 @@ var wordList = ["one", "two", "three", "four", "five"];
 var win = 0;
 var loss = 0;
 // var wordLength = unsolvedWord.length;
-var display = [underScores];
+var displayArray = [splitLetters];
 var guessesLeft = 12;
-
-var wrongLetter = [];
+var remainingLetters = 0;
+var wrongLetters = [];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var underScores = 8
+var splitLetters = 8
 var guess = " ";
-var output = " ";
+
 var unsolvedWord = "TEST";
+// var remainingLetters = splitLetters.length;
+
 // Elements are the the places where my Javascript is shown in HTML
-var showGuessesLeft = document.getElementById("guesses-left");
-var showWrongLetters = document.getElementById("wrongLetters");
-var showUnderscores = document.getElementById("under_scores");
+var showGuessesLeft = document.getElementById("guesses_left");
+var showWrongLetters = document.getElementById("wrong_letters");
+var showsplitLetters = document.getElementById("under_scores");
 
-
-
-//Start game by pressing a key?
-window.onload = function (event) {
+//Start game by on window loading
+window.onload = function (gameBegins) {
 
     // Randomly chooses a choice from the options array.
-
     unsolvedWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-    // splits the unsovledWord into individual letters and creates an underline display
+    // game mechanices set up
+    document.getElementById("guesses_left").innerHTML = guessesLeft;
 
-    var underScores = unsolvedWord.split('');
-    for (i = 0; i < underScores.length; i++) {
-        display[i] = "_  "
-        output = output + display[i];
+    // document.getElementById("wrong_letters").innerHTML = wrongLetter;
 
-
-        document.getElementById("under_scores").innerHTML = (output);
-
-
+    // splits the unsovledWord into individual letters and creates an underline displayArray
+    var splitLetters = unsolvedWord.split('');
+    for (i = 0; i < splitLetters.length; i++) {
+        displayArray[i] = "_  "
+        // output = output + displayArray[i];
     }
-    console.log(display);
-    console.log(output);
-    console.log(underScores);
-    console.log("-------------------------");
+    document.getElementById("under_scores").innerHTML = (displayArray.join(" "));
+    remainingLetters = splitLetters.length;
 
-    // other game setup mechanics
+    // user input event = letter 
+    document.onkeyup = function (event) {
+        if (event.key === wrongLetters[]) {
+            alert(POOP);
+        }
 
-    document.getElementById("guesses-left").innerHTML =
-        (guessesLeft);
+        var letterChoice = (event.key);
+        for (i = 0; i < splitLetters.length; i++) {
+            // if letter matches in word - write it down
+
+            if (splitLetters[i] === event.key) {
+                (displayArray[i] = event.key + " ");
+                remainingLetters--;
+                guessesLeft++
+                document.getElementById("under_scores").innerHTML = (displayArray.join(" "));
+            }
 
 
 
+
+        } wrongLetters.push(event.key);
+        wrongLetters.sort;
+        document.getElementById("wrong_letters").innerHTML = wrongLetters.join(" ")
+
+        document.getElementById("guesses_left").innerHTML = guessesLeft;
+
+        guessesLeft--;
+        console.log("guesses left", guessesLeft, remainingLetters);
+        console.log(letterChoice, wrongLetters.length);
+        console.log(displayArray);
+        console.log("------------");
+
+
+        console.log("displayArray, output, splitLetters, ");
+    }
 
 
 
 }
 
+
+
+        // check string - if letter is already been chosen - do nothing
+        // put chosen letter into a string
+
+        // if letter is in word array - 
+
+
+
+
+
+
+
+
+
+    // CONSOLE TEST 
+
+
+
+
+
+
+    // other game setup mechanics
+
+
+
+
+
+
+
+
+
 // var guess = onkeyup
 
 // var gameBegins = function () {
 //     for (var i = 0; i < unsolvedWord.length; i++) {
-//         showUnderscores[i] = "_ ";
-//         output = output + showUnderscores[i];
+//         showsplitLetters[i] = "_ ";
+//         output = output + showsplitLetters[i];
 
 //     }
-//     document.getElementById("underScores").innerHTML = output;
+//     document.getElementById("splitLetters").innerHTML = output;
 //     output = " ";
 // }
 
